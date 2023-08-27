15d4a4edffb9f8187fcec1ee52018e14b6796b79
kunaljain7680 <jainkunal2003@gmail.com>
2023-08-28 00:41:37 +0530

    kunal

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("7:30am");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
@@ -16,6 +16,7 @@ function select(){
    
     document.getElementById("written").style.display="flex";
     var li = document.createElement("li");
+    li.classList.add("text-list");
     var inputValue = document.getElementById("write-text").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
@@ -26,10 +27,13 @@ function select(){
     }
 
     document.getElementById("write-text").value = "";
+    document.querySelector("#oggy").style.display="span";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("7:30am");
+
   span.className = "close";
+
   span.appendChild(txt);
   span.style.color="#ffffff99";
   span.style.marginLeft="30px";
@@ -45,4 +49,33 @@ function select(){
    
 }
 
+let custom=document.querySelector(".chat-message2");
 
+
+function deletes(event){
+  const clicked=event.target;
+  console.log(clicked);
+  if(clicked.matches(".chat-message2") == true){
+    // const parent = clicked.parentElement;
+      
+    for (i=0;i<clicked.children.length;i++){
+      const ele=clicked.children[i];
+
+    ele.style.display="none";
+    document.querySelector("#oggy").style.display="none";
+    }
+    
+  }
+}
+
+custom.addEventListener("click",deletes);
+
+
+// let chatAll=document.querySelector("#full-clear");
+
+// chatAll
+
+function clearAll(){
+  document.querySelector(".chat-message-group").style.display="none";
+  document.querySelector("#chat-message-group1").style.display="none";
+}
